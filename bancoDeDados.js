0b59461be5f350b600690f5e69c0c82a5253d631
Manuel Duarte <51843064+ManuelSVDuarte@users.noreply.github.com>
2026-09-01 18:51:07 -0300

    Add simulated eye condition data to bancoDeDados.js
    This file contains simulated data for various eye conditions, including diagnostic information and associated images.

---
+++ bancoDeDados.js
@@ -0,0 +1,61 @@
+// bancoDeDados.js
+
+export const dadosSimulado = [
+  {
+    id: 1,
+    fotos: [
+      "img/foto_01.jpg"
+    ],
+    achados: "Nervo óptico normocorado e com contornos regulares, arcadas vasculares tortuosas, com cruzamento arterio venoso patologico, hemorragia de retina, fios de cobre e manchas algodonosas.",
+    diagnostico: "DM"
+  },
+  {
+    id: 2,
+    fotos: [
+      "img/foto_02.jpg"
+    ],
+    achados: "Nervo óptico normocorado e com contornos regulares, hemorragia de retina, exsudato duro, cicatriz de laser.",
+    diagnostico: "Retinopatia diabética proliferativa",
+    tratamento: "Injeção intravítrea anti-VEGF"
+  },
+  {
+    id: 3,
+    fotos: [
+      "img/foto_03.jpg"
+    ],
+    achados: "Exsudato, tortuosidade, cruzamento, hemorragia difusa, estreitamento arteriolar, contornos mal definidos do nervo óptico, achado de isquemia com a arteriografia.",
+    diagnostico: "Retinopatia hipertensiva grau 3"
+  },
+  {
+    id: 4,
+    fotos: [
+      "img/foto_04.jpg"
+    ],
+    achados: "", 
+    diagnostico: "Hiperemia conjuntival e úlcera de córnea"
+  },
+  {
+    id: 6,
+    fotos: [
+      "img/foto_06.jpg"
+    ],
+    achados: "Hemorragias em chama de vela, manchas algodonosas, fios de cobre ou prata nas arteríolas, cruzamento arteriovenoso patológico, exsudatos lipídicos e edema de disco óptico (borramento das margens).",
+    diagnostico: "Retinopatia hipertensiva maligna"
+  },
+  {
+    id: 7,
+    fotos: [
+      "img/foto_07.jpg"
+    ],
+    achados: "Hemorragia sub-retiniana e pré-retiniana.",
+    diagnostico: "Macroaneurisma arterial retiniano roto"
+  },
+  {
+    id: 8,
+    fotos: [
+      "img/foto_08.jpg"
+    ],
+    achados: "Aumento da relação escavação/disco vertical e afinamento da rima neurossensorial superior (disco óptico esquerdo).",
+    diagnostico: "Glaucoma Primário de Ângulo Aberto (GPAA)"
+  }
+];
